63b2ca99666758bb0913b9d5719775e30f2b2d29
Utku Sarioglu <utkusarioglu@hotmail.com>
2019-11-04 08:05:29 +0300

    Global declarations alterarion

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,11 @@
-export declare global {
-    interface Object {
-        sniff: (path: string | string[], absent_func?: (last_available_step: any) => any, available_func?: (last_available_step: any) => any) => any;
-        pave: (path: string | string[], occupied?: () => any, pave_success?: () => {}) => any;
-        is_empty: () => boolean;
-        is_Object: () => boolean;
-        has_Function: (func_name: string) => boolean;
+declare module "@utkusarioglu/object-assist" {
+    global {
+        interface Object {
+            sniff: (path: string | string[], absent_func?: (last_available_step: any) => any, available_func?: (last_available_step: any) => any) => any;
+            pave: (path: string | string[], occupied?: () => any, pave_success?: () => {}) => any;
+            is_empty: () => boolean;
+            is_Object: () => boolean;
+            has_Function: (func_name: string) => boolean;
+        }
     }
 }
-
-export declare const Separator: {
-    Expression: string;
-};
