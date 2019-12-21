917cba2a1081e35b344eb8c338b33ba4fc1a846d
Utku Sarioglu <utkusarioglu@hotmail.com>
2019-12-21 12:55:45 +0300

    Convention updates

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require("webpack");
 
 module.exports = {
     entry: {
-        'index': './src/object-assist.ts'
+        'index': './src/index.ts'
     },
     mode: "production",
     output: {
